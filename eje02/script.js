24d99d10ccc22875573f673f94af070d885a23da
Raul Del Castillo <rjdcv1@gmail.com>
2024-09-19 09:21:37 -0400

    cambio02

---
+++ eje02/script.js
@@ -0,0 +1,14 @@
+const OPTIONS ={
+    method:'GET',
+    headers:{
+        'X-RapidAPI-Key':
+        '71e59e906dmshfdc9ed284df2469p1b599cjsn0355f0f200d5',
+        'X-RapidAPI-Host': 'ip.geolocation-and-threat-detection.p.rapidapi.com'
+    }
+}
+
+const fechIpInfo = ip =>{
+   return fetch(`https://ip.geolocation-and-threat-detection.p.rapidapi.com/54.85.132.205${ip}`)
+   .then(res => res.json())
+   .catch(err =>console.error(err))
+}
