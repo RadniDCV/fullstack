bb8726013e8dedf65f2761d7df042daf55ad5f51
Raul Del Castillo <rjdcv1@gmail.com>
2024-09-19 10:29:15 -0400

    cambio 4

--- eje02/script.js
+++ eje02/script.js
@@ -1,21 +1,55 @@
-const OPTIONS ={
-    method:'GET',
-    headers:{
-        'X-RapidAPI-Key':
-        '71e59e906dmshfdc9ed284df2469p1b599cjsn0355f0f200d5',
-        'X-RapidAPI-Host': 'ip.geolocation-and-threat-detection.p.rapidapi.com'
+const OPTIONS = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '775533cccdmsh54e6cdf750b09b0p185a6fjsn0dee87c9ee62',
+        'x-rapidapi-host': 'ip-directory.p.rapidapi.com'
     }
 }
 
-const fechIpInfo = ip =>{
-   return fetch(`https://ip.geolocation-and-threat-detection.p.rapidapi.com/54.85.132.205${ip}`)
-   .then(res => res.json())
-   .catch(err =>console.error(err))
+const isValidIp = (ip) => {
+    const ipRegex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return ipRegex.test(ip);
 }
 
-const $form = document.querySelector('#form')
+const fetchIpInfo = ip => {
+    return fetch(`https://ip-directory.p.rapidapi.com/lookup/${ip}?risk=true&hostname=false`, OPTIONS)
+        .then(res => res.json())
+        .catch(err => {
+            console.error(err);
+            throw new Error('Error al obtener la información de la IP');
+        });
+}
 
-$form.addEventListener('submit', (event)=>{
-    event.preventDefault()
-})
+const $ = selector => document.querySelector(selector)
 
+const $form = $('#form')
+const $input = $('#input')
+const $submit = $('#submit')
+const $results = $('#results')
+
+$form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const { value } = $input;
+    
+    if (!value) return alert('Por favor, ingresa una IP');
+    if (!isValidIp(value)) return alert('Formato de IP inválido');
+
+    $submit.setAttribute('disabled', '');
+    $submit.setAttribute('aria-busy', 'true');
+    $results.textContent = 'Cargando...';
+
+    try {
+        const ipInfo = await fetchIpInfo(value);
+        
+        if (ipInfo) {
+            $results.innerHTML = JSON.stringify(ipInfo, null, 2);
+        } else {
+            $results.textContent = 'No se encontró información para esta IP.';
+        }
+    } catch (error) {
+        $results.textContent = error.message;
+    } finally {
+        $submit.removeAttribute('disabled');
+        $submit.removeAttribute('aria-busy');
+    }
+});
