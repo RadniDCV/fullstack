adb4eb5e35dd1719f9665aa4c0f12e961ea21da2
Raul Del Castillo <rjdcv1@gmail.com>
2024-09-23 08:36:07 -0400

    java01

---
+++ eje06/script.js
@@ -0,0 +1,26 @@
+let edad = 25;
+const nombre = "Nomada digital"
+
+//let: Permite declarar variables cuyo valor puede cambiar con el tiempo.
+//Su alcance se limira al bloque, declaracion, o expresion donde se utiliza
+
+//const: Se utiliza para declarar variables con un valor que no se pretende cambiar
+//Su alcance es el mismo que let
+
+let n= 123;
+console.log(n)
+
+let div = 1/0;
+console.log(div)
+
+let sum01 = (NaN + 1)
+console.log(sum01)
+
+let sum02 = 9007199254740991 + 1
+console.log(sum02)
+
+let sum03 = 9007199254740991 + 2
+console.log(sum03)
+
+let name = "John"
+console.log(`Hola, ${name}!`)
