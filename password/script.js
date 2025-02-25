427b643a132c9d8147bc0e1e2cf23fe0e746788f
Raul Del Castillo <rjdcv1@gmail.com>
2025-02-25 06:57:22 -0400

    cambio-2502

---
+++ password/script.js
@@ -0,0 +1,7 @@
+function passWord(){
+    if(x==1){
+        document.getElementById('password').type ='password';
+        document.getElementById('pass-icon').src ='hide.png';
+        x=0;
+    }
+}
