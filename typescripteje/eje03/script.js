ddde8e3b75a4d2793bb3dff2cd903f7ed4b0fd4d
Raul Del Castillo <rjdcv1@gmail.com>
2025-03-16 07:12:22 -0400

    cambio-16030

--- typescripteje/eje03/script.js
+++ typescripteje/eje03/script.js
@@ -1,6 +1,6 @@
 "use strict";
 (() => {
     const numeros = ["hola mundo", 45];
-    numeros[0] = "hola";
+    numeros[0] = "hola como estas";
     console.log(numeros);
 })();
--- typescripteje/eje03/script.ts
+++ typescripteje/eje03/script.ts
@@ -1,5 +1,5 @@
 (()=>{
   const numeros:[string, number]=["hola mundo", 45]
-  numeros[0]="hola"
+  numeros[0]="hola como estas"
   console.log(numeros)  
 })()
